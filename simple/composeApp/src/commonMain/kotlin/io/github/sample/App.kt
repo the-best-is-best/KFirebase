b4cc41b1acd804f3df137a8e6d87d6c6c1292bff
Michelle Raouf <michelle.raouf@52ndsolution.net>
2024-10-22 10:30:03 +0300

    database check listener

--- simple/composeApp/src/commonMain/kotlin/io/github/sample/App.kt
+++ simple/composeApp/src/commonMain/kotlin/io/github/sample/App.kt
@@ -11,19 +11,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import io.github.firebase_core.KFirebaseCore
-import io.github.firebase_storage.KFirebaseStorage
+import io.github.firebase_database.KFirebaseDatabase
 import io.github.sample.theme.AppTheme
-import io.github.vinceglb.filekit.compose.rememberFilePickerLauncher
-import io.github.vinceglb.filekit.core.FileKit
-import io.github.vinceglb.filekit.core.PickerMode
-import io.github.vinceglb.filekit.core.PickerType
-import io.github.vinceglb.filekit.core.PlatformFile
-import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 
 @Composable
@@ -32,24 +26,85 @@ internal fun App() = AppTheme {
 
     val app = KFirebaseCore.app()
     println(app.options) // Check this log
-    val storage = KFirebaseStorage()
+    val db = KFirebaseDatabase()
 
-    var imageUploadedPath: String? = null
+    val books = mutableListOf(
+        mapOf(
+            "id" to 1,
+            "title" to "To Kill a Mockingbird",
+            "author" to mapOf(
+                "name" to "Harper Lee",
+                "bio" to "American novelist",
+                "birthYear" to 1926
+            ),
+            "genre" to "Fiction",
+            "publishedYear" to 1960,
+            "isbn" to "978-0-06-112008-4",
+            "rating" to 4.27,
+            "description" to "A novel about the serious issues of rape and racial inequality seen through the eyes of a child."
+        ),
+        mapOf(
+            "id" to 2,
+            "title" to "1984",
+            "author" to mapOf(
+                "name" to "George Orwell",
+                "bio" to "English novelist and essayist",
+                "birthYear" to 1903
+            ),
+            "genre" to "Dystopian",
+            "publishedYear" to 1949,
+            "isbn" to "978-0-452-28423-4",
+            "rating" to 4.17,
+            "description" to "A novel that presents a dystopian future under a totalitarian regime where surveillance and manipulation are commonplace."
+        ),
+        mapOf(
+            "id" to 3,
+            "title" to "The Great Gatsby",
+            "author" to mapOf(
+                "name" to "F. Scott Fitzgerald",
+                "bio" to "American novelist",
+                "birthYear" to 1896
+            ),
+            "genre" to "Fiction",
+            "publishedYear" to 1925,
+            "isbn" to "978-0-7432-7356-5",
+            "rating" to 3.91,
+            "description" to "A critique of the American Dream, exploring themes of wealth, love, and the pursuit of happiness in the Jazz Age."
+        ),
+        mapOf(
+            "id" to 4,
+            "title" to "Pride and Prejudice",
+            "author" to mapOf(
+                "name" to "Jane Austen",
+                "bio" to "English novelist",
+                "birthYear" to 1775
+            ),
+            "genre" to "Romance",
+            "publishedYear" to 1813,
+            "isbn" to "978-1-85326-000-1",
+            "rating" to 4.26,
+            "description" to "A romantic novel that critiques the British landed gentry at the end of the 18th century."
+        ),
+        mapOf(
+            "id" to 5,
+            "title" to "The Catcher in the Rye",
+            "author" to mapOf(
+                "name" to "J.D. Salinger",
+                "bio" to "American writer",
+                "birthYear" to 1919
+            ),
+            "genre" to "Fiction",
+            "publishedYear" to 1951,
+            "isbn" to "978-0-316-76948-0",
+            "rating" to 3.81,
+            "description" to "A story about teenage angst and alienation, narrated by the iconic character Holden Caulfield."
+        )
+    )
 
-    val path = "images/"
 
-    var selectedFile: PlatformFile? = null
-    val scope = rememberCoroutineScope()
-// FileKit Compose
-    val launcher = rememberFilePickerLauncher(
-        type = PickerType.ImageAndVideo,
-        mode = PickerMode.Single,
-        title = "Pick a media",
-        initialDirectory = "/custom/initial/path"
-    ) { file ->
-        selectedFile = file
-        print("file selected")
-    }
+    val path = "books"
+
+
 
 
     LazyColumn(
@@ -61,79 +116,61 @@ internal fun App() = AppTheme {
     ) {
         item {
 
+
+            ElevatedButton(
+                onClick = {
+                    db.writeList(path, books) {
+                        it.onSuccess {
+                            println("success add books")
+                        }
+                    }
+
+                }) {
+                Text("Add list dummy book data")
+            }
+            Spacer(Modifier.height(20.dp))
+          ElevatedButton(onClick = {
+              db.addObserveListener(path){
+                  it.onSuccess {
+                      println("new value in listener $it")
+                  }
+                  it.onFailure {
+                      println("new value in listener error $it")
+                  }
+              }
+          }){
+              Text("Listener")
+          }
+            Spacer(Modifier.height(20.dp))
             ElevatedButton(onClick = {
-                launcher.launch()
-
-            }) {
-                Text("Select image")
-            }
-            ElevatedButton(
-                onClick = {
-                    if (selectedFile != null) {
-                        scope.launch {
-                            val bytes = selectedFile!!.readBytes()
-                            storage.uploadFile("$path/${selectedFile!!.name}", bytes) {
-                                it.onSuccess { (url, filePath) ->
-                                    imageUploadedPath = filePath
-                                }
-                            }
-                        }
+                val newId = Random.nextInt(books.lastIndex, books.lastIndex +1)
+                val newBook =  mapOf(
+                    "id" to newId,
+                    "title" to "The Catcher in the Rye",
+                    "author" to mapOf(
+                        "name" to "J.D. Salinger",
+                        "bio" to "American writer",
+                        "birthYear" to 1919
+                    ),
+                    "genre" to "Fiction",
+                    "publishedYear" to 1951,
+                    "isbn" to "978-0-316-76948-0",
+                    "rating" to 3.81,
+                    "description" to "A story about teenage angst and alienation, narrated by the iconic character Holden Caulfield."
+                )
+                books.add(newBook)
+                db.write("$path/$newId", newBook){
+                    it.onSuccess {
+                        println("data write")
                     }
-
-
-                }) {
-                Text("Upload image")
-            }
-            Spacer(Modifier.height(20.dp))
-
-            ElevatedButton(
-
-                onClick = {
-                    if (imageUploadedPath != null) {
-                        storage.downloadFile(imageUploadedPath!!) {
-                            it.onSuccess {
-                                scope.launch {
-
-                                    println("Size downloaded ${it?.fileBytes?.size}")
-                                    val supported = FileKit.isSaveFileWithoutBytesSupported()
-                                    println("support save $supported")
-                                    if (supported && it != null) {
-                                        val fileSaved = FileKit.saveFile(
-                                            it.fileBytes,
-                                            extension = it.fileExtension,
-                                            baseName = it.fileName
-                                        )
-                                        println("file saved path is ${fileSaved?.path}")
-                                    }
-                                }
-                            }
-                        }
+                    it.onFailure {
+                        println("data failed write")
                     }
-                }) {
-                Text("Download image")
+                }
+            }){
+
+                Text("add value")
             }
-            Spacer(Modifier.height(20.dp))
-
-            ElevatedButton(
-                onClick = {
-                    if (imageUploadedPath != null) {
-
-                        storage.deleteFile(imageUploadedPath!!) {
-                            println("image path ${imageUploadedPath}")
-                            it.onSuccess {
-                                println("deleted file")
-                                imageUploadedPath = null
-                            }
-                            it.onFailure {
-                                println("error delete $it")
-                            }
-                        }
-
-                    }
-                }) {
-                Text("delete image")
-            }
-
         }
 
 
