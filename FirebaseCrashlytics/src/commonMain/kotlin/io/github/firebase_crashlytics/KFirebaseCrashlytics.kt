238a290a337a42fa8713a53137d38c01b7da2c17
Michelle Raouf <michelle.raouf@52ndsolution.net>
2025-03-09 00:05:51 +0200

    Add TouchLabCrashlytics submodule and update KFirebaseCore and KFirebaseAnalytics references; remove deprecated platform-specific implementations

--- FirebaseCrashlytics/src/commonMain/kotlin/io/github/firebase_crashlytics/KFirebaseCrashlytics.kt
+++
@@ -1,7 +0,0 @@
-package io.github.firebase_crashlytics
-
-expect class KFirebaseCrashlytics() {
-    fun setCrashlyticsCollectionEnabled(enable: Boolean)
-    fun log(msg: String)
-    fun recordError(exception: Throwable)
-}
