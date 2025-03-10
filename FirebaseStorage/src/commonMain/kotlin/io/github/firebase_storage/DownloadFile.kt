2130c16c21de19afbb7aa7da1792754e51436cb7
Michelle Raouf <michelle.raouf@52ndsolution.net>
2025-03-10 16:29:41 +0200

    Remove deprecated files and add KFirebaseMessaging submodule

--- FirebaseStorage/src/commonMain/kotlin/io/github/firebase_storage/DownloadFile.kt
+++
@@ -1,7 +0,0 @@
-package io.github.firebase_storage
-
-data class KFirebaseStorageDownloadedFile(
-    val fileName: String,
-    val fileExtension: String,
-    val fileBytes: ByteArray
-)
--- simple/composeApp/src/iosMain/kotlin/main.kt
+++
@@ -1,7 +0,0 @@
-import androidx.compose.ui.window.ComposeUIViewController
-import io.github.sample.AppFCM
-import platform.UIKit.UIViewController
-
-fun MainViewController(): UIViewController {
-    return ComposeUIViewController { AppFCM() }
-}
